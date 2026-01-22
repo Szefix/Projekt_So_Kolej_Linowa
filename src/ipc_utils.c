91e99e0072cf0acc8f72c4eed7e6ddf385bdf2bc
Szefix <kamil.kedra2004@gmail.com>
2026-01-22 12:15:22 +0100

    dodanie pamieci wspoldzielonej

--- src/ipc_utils.c
+++ src/ipc_utils.c
@@ -199,3 +199,98 @@ void usun_semafory(Semafory *sem) {
         sem_unlink(nazwa);
     }
 }
+
+/* ========== INICJALIZACJA PAMIĘCI WSPÓŁDZIELONEJ ========== */
+
+int inicjalizuj_pamiec_wspoldzielona(PamiecWspoldzielona *shm) {
+    /* Usuń starą pamięć jeśli istnieje */
+    int stary_shm = shmget(SHM_KEY, sizeof(StanWspoldzielony), 0666);
+    if (stary_shm != -1) {
+        shmctl(stary_shm, IPC_RMID, NULL);
+    }
+    
+    shm->shm_id = shmget(SHM_KEY, sizeof(StanWspoldzielony), IPC_CREAT | IPC_EXCL | 0666);
+    if (shm->shm_id == -1) {
+        perror("shmget create");
+        return -1;
+    }
+    
+    shm->stan = (StanWspoldzielony *)shmat(shm->shm_id, NULL, 0);
+    if (shm->stan == (void *)-1) {
+        perror("shmat");
+        return -1;
+    }
+    
+    /* Inicjalizacja stanu początkowego */
+    memset(shm->stan, 0, sizeof(StanWspoldzielony));
+    
+    shm->stan->kolej_aktywna = true;
+    shm->stan->kolej_zatrzymana = false;
+    shm->stan->godziny_pracy = true;
+    shm->stan->czas_startu = time(NULL);
+    shm->stan->nastepny_turysta_id = 1;
+    shm->stan->nastepny_bilet_id = 1;
+    shm->stan->liczba_osob_na_stacji = 0;
+    shm->stan->liczba_osob_na_peronie = 0;
+    shm->stan->liczba_aktywnych_krzeselek = 0;
+    shm->stan->laczna_liczba_zjazdow = 0;
+    shm->stan->liczba_sprzedanych_biletow = 0;
+    shm->stan->liczba_wpisow_rejestru = 0;
+    shm->stan->kto_zatrzymal = 0;
+    
+    /* Inicjalizacja bramek wejściowych */
+    for (int i = 0; i < LICZBA_BRAMEK_WEJSCIOWYCH; i++) {
+        shm->stan->bramki_wejsciowe[i].id = i;
+        shm->stan->bramki_wejsciowe[i].otwarta = true;
+        shm->stan->bramki_wejsciowe[i].aktualny_turysta_id = -1;
+    }
+    
+    /* Inicjalizacja bramek peronowych */
+    for (int i = 0; i < LICZBA_BRAMEK_PERONOWYCH; i++) {
+        shm->stan->bramki_peronowe[i].id = i;
+        shm->stan->bramki_peronowe[i].otwarta = false;
+        shm->stan->bramki_peronowe[i].aktualny_turysta_id = -1;
+    }
+    
+    /* Inicjalizacja krzesełek */
+    for (int i = 0; i < MAX_AKTYWNYCH_KRZESELEK; i++) {
+        shm->stan->krzeselka[i].id = i;
+        shm->stan->krzeselka[i].aktywne = false;
+        shm->stan->krzeselka[i].liczba_pasazerow = 0;
+        shm->stan->krzeselka[i].liczba_rowerzystow = 0;
+        for (int j = 0; j < POJEMNOSC_KRZESELKA; j++) {
+            shm->stan->krzeselka[i].pasazerowie[j] = -1;
+        }
+    }
+    
+    return 0;
+}
+
+/* ========== ŁĄCZENIE Z PAMIĘCIĄ WSPÓŁDZIELONĄ ========== */
+
+int polacz_pamiec_wspoldzielona(PamiecWspoldzielona *shm) {
+    shm->shm_id = shmget(SHM_KEY, sizeof(StanWspoldzielony), 0666);
+    if (shm->shm_id == -1) {
+        perror("shmget connect");
+        return -1;
+    }
+    
+    shm->stan = (StanWspoldzielony *)shmat(shm->shm_id, NULL, 0);
+    if (shm->stan == (void *)-1) {
+        perror("shmat connect");
+        return -1;
+    }
+    
+    return 0;
+}
+
+/* ========== USUWANIE PAMIĘCI WSPÓŁDZIELONEJ ========== */
+
+void usun_pamiec_wspoldzielona(PamiecWspoldzielona *shm) {
+    if (shm->stan && shm->stan != (void *)-1) {
+        shmdt(shm->stan);
+    }
+    if (shm->shm_id != -1) {
+        shmctl(shm->shm_id, IPC_RMID, NULL);
+    }
+}
